5cabe6f90cfc8ff6e30810cf4efd1e0222ecfc2f
ibrahim <ibrodjabi@yahoo.fr>
2020-07-26 04:43:53 +0000

    Initial commit

---
+++ app/src/main/java/com/project/easytransportapp/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.project.easytransportapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.easytransportapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
